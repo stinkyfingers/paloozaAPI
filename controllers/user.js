24820da626563a4648c5b6d2901fb29d1cd91870
stinkyfingers <john_shenk@hotmail.com>
2015-09-07 22:05:27 -0500

    create user query

--- controllers/user.js
+++ controllers/user.js
@@ -9,19 +9,29 @@ var user = require('../models/user'),
 exports.create = function(req, res) {
 	var u = new user(req.body);
 	u.password = md5(u.password);
-	u.save(function(err){
+	user.create(u, function(err){
 		if (err) throw err;
 	});
+	u.password = null;
 	res.json(u);
 };
 
 exports.update = function(req, res) {
 	var u = new user(req.body);
-	u.password = md5(u.password);
+	var query  = {
+		username: u.username,
+		firstName: u.firstName,
+		lastName: u.lastName,
+		color: u.color,
+	};
+	if (u.password !== null && u.password !== ''){
+		query.password = md5(u.password);
+	}
 	u.isNew = false;
-	u.save(function(err){
+	user.update({id:u._id}, query, function(err){
 		if (err) throw err;
 	});
+	u.password = null;
 	res.json(u);
 };
 
@@ -29,6 +39,7 @@ exports.get = function(req, res) {
 	var id = req.params.id;
 	user.findById(id).then(function(u){
 		if (u.length > 0){
+			u[0].password = null;
 			res.json(u[0]);
 			return;
 		}
@@ -39,11 +50,12 @@ exports.get = function(req, res) {
 exports.authenticate = function(req, res) {
 	var u = new user(req.body);
 	u.password = md5(u.password);
-	user.findOne({'username':u.username, 'password':u.password}).then(function(user){
-		if (!user){
+	user.findOne({username:u.username, password:u.password}, function(err, user){
+		if (!user || err){
 			res.send(new restify.errors.UnauthorizedError);
 			return;
 		}
+		user.token = null;
 		var token = jwt.sign(user, utility.config.secret, {
           expiresInMinutes: 1440 // expires in 24 hours
         });
@@ -52,6 +64,8 @@ exports.authenticate = function(req, res) {
         user.save(function(err){
         	if (err) return err;
         });
+        user.password = null;
+
 		res.json({
           success: true,
           message: 'Enjoy your token!',
@@ -65,6 +79,9 @@ exports.getAll = function(req, res) {
 	user.find({}, function(err, us){
 		if (err) throw err;
 	}).then(function(us){
+		for (u of us){
+			u.password = null
+		}
 		res.json(us);
 	});
 };
